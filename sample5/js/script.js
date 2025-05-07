455dfcedec796e4f9e3961a1342fd7bf4a9443c0
codewala-feed <codewala.info@gmail.com>
2025-05-07 11:15:08 +0530

    day 15

---
+++ sample5/js/script.js
@@ -0,0 +1,9 @@
+function pressed(){
+    let b1 = document.getElementById("tb1");
+    b1.style.boxShadow = "10px 10px 10px black";
+}
+
+function released(){
+    let b1 = document.getElementById("tb1");
+    b1.style.boxShadow = "";
+}
